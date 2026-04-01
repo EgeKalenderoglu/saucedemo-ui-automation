0bb7bed119794d3e1929e91f0727f424e6e569b2
Ege Kalenderoglu <egekalenderoglu@outlook.com>
2026-04-01 18:59:57 -0400

    Add WebDriverWait to inventory_page.py and fix fragile locator

--- pages/inventory_page.py
+++ pages/inventory_page.py
@@ -1,18 +1,21 @@
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 class InventoryPage:
 
     def __init__(self, driver):
         self.driver = driver
+        self.wait = WebDriverWait(driver, 10)
 
 
     def add_first_product_to_cart(self):
-        self.driver.find_element(By.CSS_SELECTOR, ".inventory_item button").click()
+        self.wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, "[data-test='add-to-cart-sauce-labs-backpack']"))).click()
 
     def get_cart_count(self):
-        return self.driver.find_element(By.CLASS_NAME, "shopping_cart_badge").text
+        return self.wait.until(EC.visibility_of_element_located((By.CLASS_NAME, "shopping_cart_badge"))).text
 
     def go_to_cart(self):
-        self.driver.find_element(By.CLASS_NAME, "shopping_cart_link").click()
+        self.wait.until(EC.element_to_be_clickable((By.CLASS_NAME, "shopping_cart_link"))).click()
 
         
