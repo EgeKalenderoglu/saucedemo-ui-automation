0edc6df1976ebdb1a5bf8a2c8320af0fae416c21
Ege Kalenderoglu <egekalenderoglu@outlook.com>
2026-04-01 19:21:56 -0400

    Add WebDriverWait to cart_page.py and checkout_complete_page.py

--- pages/cart_page.py
+++ pages/cart_page.py
@@ -1,13 +1,16 @@
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 class CartPage:
     def __init__(self, driver):
         self.driver = driver
+        self.wait = WebDriverWait(driver, 10)
 
     def get_cart_items_count(self):
-        items = self.driver.find_elements(By.CLASS_NAME, "cart_item")
+        items = self.wait.until(EC.visibility_of_all_elements_located((By.CLASS_NAME, "cart_item")))
         return len(items)
 
     def click_checkout_button(self):
-        self.driver.find_element(By.ID, "checkout").click()
+        self.wait.until(EC.element_to_be_clickable((By.ID, "checkout"))).click()
     
--- pages/checkout_complete_page.py
+++ pages/checkout_complete_page.py
@@ -1,8 +1,11 @@
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 class CheckOutCompletePage:
     def __init__(self, driver):
         self.driver = driver
+        self.wait = WebDriverWait(driver, 10)
 
     def get_success_message(self):
-        return self.driver.find_element(By.CLASS_NAME, "complete-header").text
+        return self.wait.until(EC.visibility_of_element_located((By.CLASS_NAME, "complete-header"))).text
